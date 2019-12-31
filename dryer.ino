fe1627aa91d4763f9d9c2952220cf10dd1a4e048
Sachin Prabhu <sprabhu@redhat.com>
2019-12-31 22:28:31 +0000

    Connect to Wifi using WiFiManager

--- dryer.ino
+++ dryer.ino
@@ -1,9 +1,23 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>
+
 #include "globals.h"
 
 void setup() {
     Serial.begin(9600);
     delay(100);
     Serial.println("Starting!");
+
+    WiFiManager wm;
+    if (!wm.autoConnect()) {
+        dbg_print("Timed out waiting for Wifi. Putting to sleep");
+        ESP.reset();
+        delay(1000);
+    }
+
+    dbg_print("Connected to Wifi");
 }
 
 void loop() {
