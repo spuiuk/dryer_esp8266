feb2543eb6ef18ff7e2593a0ed457e383bc7dfde
Sachin Prabhu <sprabhu@redhat.com>
2020-01-01 00:59:52 +0000

    Allow new parameters to be added

--- dryer.ino
+++ dryer.ino
@@ -1,20 +1,112 @@
+#include <FS.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
 #include <WiFiManager.h>
+#include <ArduinoJson.h>
 
 #include "globals.h"
 
+bool shouldSaveConfig = false;
+
+/* add default values for your parameters here */
+/* char token[34] = "YourSecurityToken"; */
+
+bool readConfigFile(void)
+{
+    File configFile = SPIFFS.open("/config.json", "r");
+    if (!configFile) {
+        Serial.println("Failed to read config");
+        return false;
+    }
+
+    size_t size = configFile.size();
+    std::unique_ptr<char[]> buf(new char[size]);
+    configFile.readBytes(buf.get(), size);
+
+    StaticJsonDocument<512> doc;
+    auto error = deserializeJson(doc, buf.get());
+    if (error) {
+        Serial.println("Could not parse config file");
+        configFile.close();
+        return false;
+    }
+
+    /* Copy your parameter value from json */
+    /* strcpy(token, json["token"]); */
+
+    configFile.close();
+    return true;
+}
+
+bool saveConfigFile(void)
+{
+    StaticJsonDocument<512> doc;
+
+    /* Copy over parameters to json object */
+    /* doc[token] = token; */
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if(!configFile) {
+        Serial.println("Failed to open configfile for writing");
+        return false;
+    }
+
+    serializeJson(doc, configFile);
+    configFile.close();
+
+    return true;
+}
+
+void saveConfigCallback()
+{
+    dbg_print("Save Config Callback");
+    shouldSaveConfig = true;
+}
+
 void setup() {
-    Serial.begin(9600);
-    delay(100);
+    WiFiManager wm;
+
+    Serial.begin(115200);
+    delay(1000);
     Serial.println("Starting!");
 
-    WiFiManager wm;
+    Serial.println("Waiting 3 seconds - Press flash button(GPIO0) to reset settings");
+    delay(3000);
+    pinMode(0, INPUT);
+    if (digitalRead(0) == LOW) {
+        Serial.println("Resetting Settings");
+        SPIFFS.format();
+        wm.resetSettings();
+    }
+
+    dbg_print("Attempting to mount fs");
+    if (!SPIFFS.begin()) {
+        Serial.println("Failed to mount fs.");
+        return;
+    }
+
+    if (!readConfigFile()) {
+        Serial.println("Could not read config file");
+    }
+
+    /* Add custom parameters here */
+    /* WiFiManagerParameter custom_token("Token", token, 40); */
+
+    wm.setSaveConfigCallback(saveConfigCallback);
     if (!wm.autoConnect()) {
         dbg_print("Timed out waiting for Wifi. Putting to sleep");
         ESP.reset();
         delay(1000);
+    }
+
+    /* Add customer parameter objects to WiFiManager */
+    /* wm.addParameter(&custom_token); */
+
+    if (shouldSaveConfig && !saveConfigFile()) {
+        Serial.println("Could not save config file");
+        delay(10000);
+        return;
     }
 
     dbg_print("Connected to Wifi");
